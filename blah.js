9eec95c6d7f852af234d603c37036b0c6d2cf9eb
Charles Goggins <c_goggz56@live.com>
2014-04-17 20:07:31 -0400

    Coming along quite nicely

---
+++ blah.js
@@ -0,0 +1,13 @@
+//Get a handle to the canvas object
+var canvas = document.getElementById("pcBuilder");
+
+//Get the 3d context for this canvas
+
+var context = canvas.getContext('experimental-webgl');
+
+var img = new Image();
+img.onload = function(); {
+	var ctx.drawImage(img, 0, 0)
+};
+	
+img.src="images/Antec.png"
